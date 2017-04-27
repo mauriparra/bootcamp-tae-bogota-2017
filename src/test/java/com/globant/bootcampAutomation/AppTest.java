65d7efec5f61c148c9dba0a31f68603c8008f292
Carlos Rios <carrio1@cobogpgp8445.global.publicisgroupe.net>
2017-04-27 17:14:13 -0500

    basic math test

--- src/test/java/com/globant/bootcampAutomation/AppTest.java
+++ src/test/java/com/globant/bootcampAutomation/AppTest.java
@@ -10,11 +10,39 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
+    int num1 = 3; int num2 = 5; int num3 = 8; int num4 = 16;
+       @org.testng.annotations.Test
+       public void sumTest() {
+           assertEquals(13,num2 + num3);
+       }
+
+       @org.testng.annotations.Test
+       public void subTest() {
+           assertEquals(2,num2 - num1);
+       }
+
+       @org.testng.annotations.Test
+       public void timesTest() {
+           assertEquals(15, num1 * num2);
+       }
+
+       @org.testng.annotations.Test
+       public void divTest() {
+           assertEquals(2,num4 / num3);
+       }
+
+       @org.testng.annotations.Test
+       public void maxTest() {
+           assertEquals(15,num2 + num4 - num1 * 2);
+       }
+
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
+
+
     public AppTest( String testName )
     {
         super( testName );
@@ -27,6 +55,7 @@ public class AppTest
     {
         return new TestSuite( AppTest.class );
     }
+
 
     /**
      * Rigourous Test :-)
