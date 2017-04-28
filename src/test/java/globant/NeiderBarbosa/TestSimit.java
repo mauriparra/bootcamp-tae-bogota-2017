906c402f3b5c0675adc01f37a0eef43b7983deaf
Neider Barbosa <Neider>
2017-04-27 19:22:14 -0500

    segundo test

--- src/test/java/globant/NeiderBarbosa/TestSimit.java
+++ src/test/java/globant/NeiderBarbosa/TestSimit.java
@@ -1,5 +1,31 @@
 package globant.NeiderBarbosa;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+
 public class TestSimit {
+	
+	@Test
+	public void testSimit() {
+	  System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://www2.simit.org.co/Simit/");
+	  driver.quit();
+	}
+	
+	@Test 
+	public void testDocument(){
+		System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+		  WebDriver driver = new ChromeDriver();
+		  driver.get("http://www2.simit.org.co/Simit/");
+		  Select comboBox = new Select(driver.findElement(By.name("tipoDocumento")));
+		  comboBox.selectByVisibleText("cedula");
+		  driver.quit();
+	}
 
 }
