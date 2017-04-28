5030706291038e43dbed8a2d29c362820fc3f4ff
Neider Barbosa <Neider>
2017-04-27 19:59:35 -0500

    final

--- src/test/java/globant/NeiderBarbosa/TestSimit.java
+++ src/test/java/globant/NeiderBarbosa/TestSimit.java
@@ -10,22 +10,40 @@ import org.testng.annotations.Test;
 
 public class TestSimit {
 	
-	@Test
-	public void testSimit() {
-	  System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+	@Test(priority = 1)
+	public void testconsultar() {
+	  //System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
 	  WebDriver driver = new ChromeDriver();
 	  driver.get("http://www2.simit.org.co/Simit/");
-	  driver.quit();
+	  WebElement pdf = driver.findElement(By.id("slideMenu_0"));
+	  pdf.click();
 	}
 	
-	@Test 
+	@Test(priority = 2)
+	public void testPdf() {
+	  //System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://www2.simit.org.co/Simit/");
+	  WebElement pdf = driver.findElement(By.name("consultar"));
+	  pdf.click();
+	}
+	
+	@Test(priority = 3) 
 	public void testDocument(){
-		System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+		//System.setProperty("webdriver.chrome.driver", "‪C:\\chromedriver.exe");
 		  WebDriver driver = new ChromeDriver();
 		  driver.get("http://www2.simit.org.co/Simit/");
-		  Select comboBox = new Select(driver.findElement(By.name("tipoDocumento")));
-		  comboBox.selectByVisibleText("cedula");
-		  driver.quit();
+		  Select TypeDoc = new Select(driver.findElement(By.name("tipoDocumento")));
+		  TypeDoc.selectByVisibleText("cedula");
+		  WebElement Number = driver.findElement(By.name("identificacion"));
+		  Number.sendKeys("1030586558");
+		  Select Type = new Select((WebElement) driver.findElements(By.name("raddiobuuton")));
+		  Type.selectByValue("5");
+		  WebElement Capchat = driver.findElement(By.name("txtInput"));
+		  Capchat.sendKeys("basura");
+		  WebElement Query = driver.findElement(By.name("consultar"));
+		  Query.click();
+		  
 	}
 
 }
