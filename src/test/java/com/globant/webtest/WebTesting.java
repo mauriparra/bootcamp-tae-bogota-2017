79335d4985e6c4241549935b6e985780e315911c
Fabian Acero <fabian.acero@decameron.com>
2017-04-28 16:56:36 -0500

    Finalizo la prueba exitosa con un assertion

--- src/test/java/com/globant/webtest/WebTesting.java
+++ src/test/java/com/globant/webtest/WebTesting.java
@@ -6,16 +6,23 @@
 package com.globant.webtest;
 
 import static com.globant.webtest.Constants.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 /**
  *
@@ -33,19 +40,37 @@ public class WebTesting {
     //
     @Test
     public void goToMakeTest() {
-        driver.findElement(By.id("navBooking")).click();
+
         WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
-        WebElement inputName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("origenCompAereo")));
-        inputName.sendKeys("Bogota.COLOMBIA");
-        //WebElement hotelSelector = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[id=hotelWrapperHotel1]>button")));
-        //hotelSelector.click();
-        //driver.findElement(By.cssSelector("div[id=hotelWrapperHotel1]>button")).click();
-        driver.findElement(By.id("entradaCompAereo1")).sendKeys("Mar, 16 May 2017");
-        driver.findElement(By.id("salidaCompAereo1")).sendKeys("Mar, 18 May 2017");
-        Select selectAdults = new Select(driver.findElement(By.id("selectAereoHab1Adultos")));
-        selectAdults.selectByValue("2");
-        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("buscarAereo")));
-        submitButton.click();
+        
+        driver.findElement(By.id("sigAge")).sendKeys(AGENCY);
+        driver.findElement(By.cssSelector("#divCiud>input")).sendKeys("Bogota.COLOMBIA");
+        String scriptText = "document.querySelector('#CIUDAD_ORIGEN').setAttribute('value','" + CITY_VALUE + "')";
+        ((JavascriptExecutor)driver).executeScript(scriptText);
+        (new Select(driver.findElement(By.id("hotel")))).selectByValue(HOTEL_VALUE);
+        (new Select(driver.findElement(By.id("hab1adultos")))).selectByValue("3");
+        
+        String[][] calendarData = new String[][]{
+            {"FECHA_LLEGADA", START_DATE},
+            {"FECHA_SALIDA", END_DATE}
+        };
+                
+        for(String[] date: calendarData){
+            driver.findElement(By.name(date[0])).click();
+            WebElement calendar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ui-datepicker-div")));
+            List<WebElement> calendarColums = calendar.findElements(By.tagName("td"));
+            for (WebElement cell: calendarColums){
+                if (cell.getText().equals(date[1])){
+                    cell.findElement(By.tagName("a")).click();
+                    break;
+                }
+            }
+        }
+
+        driver.findElement(By.className("botonNaranjaFlecha")).click();
+        WebElement loaderSpinner = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("SpanLabelDivLoad")));
+        wait.until(ExpectedConditions.invisibilityOf(loaderSpinner));
+        Assert.assertEquals(RESULTS_TITLE, driver.getTitle());
     }
 
     @BeforeMethod(description = "Opens the Web Browser")
