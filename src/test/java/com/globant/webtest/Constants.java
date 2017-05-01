e2251628fccca214fe37cbf6a2e503cf4f6e8cac
fabian.acero <acero01@gmail.com>
2017-05-01 17:36:15 -0500

    Comienzo a utilizar el patron PageFactory

--- src/test/java/com/globant/webtest/Constants.java
+++ src/test/java/com/globant/webtest/Constants.java
@@ -11,12 +11,14 @@ package com.globant.webtest;
  */
 public final class Constants {
     //public static final String URL = "https://www.decameron.com/es/co-inicio";
-    public static final String URL = "http://agencias.decameron.com/promosite/index.php/es/agencias-colombia";
+    /*public static final String URL = "http://agencias.decameron.com/promosite/index.php/es/agencias-colombia";
     public static final int TIMEOUT = 10;
     public static final String AGENCY = "A001";
     public static final String CITY_VALUE = "Bogota|3537|BOG|CO";
     public static final String HOTEL_VALUE = "3552|CARTAGENA (RAFAEL NUNEZ)|42|COLOMBIA|8670|Cartagena, Bolivar - Decameron Cartagena||CTG";
     public static final String START_DATE = "15";
     public static final String END_DATE = "18";
-    public static final String RESULTS_TITLE = "Booking Agencias CO";
+    public static final String RESULTS_TITLE = "Booking Agencias CO";*/
+    public static final String URL = "https://www.despegar.com.co/";
+    public static final String DESTINATION_NAME = "San Andrés, Providencia y Santa Catalina, Archipiélago de San Andrés, Colombia";
 }
--- src/test/java/com/globant/webtest/WebTesting.java
+++ src/test/java/com/globant/webtest/WebTesting.java
@@ -5,24 +5,14 @@
  */
 package com.globant.webtest;
 
+import com.globant.pages.HomePage;
 import static com.globant.webtest.Constants.*;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 
 /**
  *
@@ -35,52 +25,21 @@ public class WebTesting {
     public WebTesting() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    @Test
-    public void goToMakeTest() {
-
-        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
-        
-        driver.findElement(By.id("sigAge")).sendKeys(AGENCY);
-        driver.findElement(By.cssSelector("#divCiud>input")).sendKeys("Bogota.COLOMBIA");
-        String scriptText = "document.querySelector('#CIUDAD_ORIGEN').setAttribute('value','" + CITY_VALUE + "')";
-        ((JavascriptExecutor)driver).executeScript(scriptText);
-        (new Select(driver.findElement(By.id("hotel")))).selectByValue(HOTEL_VALUE);
-        (new Select(driver.findElement(By.id("hab1adultos")))).selectByValue("3");
-        
-        String[][] calendarData = new String[][]{
-            {"FECHA_LLEGADA", START_DATE},
-            {"FECHA_SALIDA", END_DATE}
-        };
-                
-        for(String[] date: calendarData){
-            driver.findElement(By.name(date[0])).click();
-            WebElement calendar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ui-datepicker-div")));
-            List<WebElement> calendarColums = calendar.findElements(By.tagName("td"));
-            for (WebElement cell: calendarColums){
-                if (cell.getText().equals(date[1])){
-                    cell.findElement(By.tagName("a")).click();
-                    break;
-                }
-            }
-        }
-
-        driver.findElement(By.className("botonNaranjaFlecha")).click();
-        WebElement loaderSpinner = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("SpanLabelDivLoad")));
-        wait.until(ExpectedConditions.invisibilityOf(loaderSpinner));
-        Assert.assertEquals(RESULTS_TITLE, driver.getTitle());
-    }
-
-    @BeforeMethod(description = "Opens the Web Browser")
+    @BeforeMethod(description = "All actions before testing")
     public void setUpMethod() throws Exception {
         driver = new ChromeDriver();
         driver.get(URL);
     }
 
-    @AfterMethod(description = "Closes the Web Browser")
+    @AfterMethod(description = "All actions after testing")
     public void tearDownMethod() throws Exception {
-        driver.quit();
+       // driver.quit();
+    }
+    
+    @Test(description = "Starts all the web test")
+    public void goToMakeTest() {
+       
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        homePage.fillBookingEngine();
     }
 }
