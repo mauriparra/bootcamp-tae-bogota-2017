32a22dfd6eac045499b7d7e669a27882bbecb086
Carlos Rios <carrio1@cobogpgp8445.global.publicisgroupe.net>
2017-05-02 00:12:26 -0500

    form submit test happy path

--- src/test/java/com/globant/bootcampAutomation/SeleniumTest.java
+++ src/test/java/com/globant/bootcampAutomation/SeleniumTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -16,11 +17,35 @@ public class SeleniumTest {
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://www.hazunaweb.com/curso-de-html/ejemplo-formulario-html/");
+        driver.get("http://www.iberia.com/co/");
         new WebDriverWait(driver,20);
-        WebElement elementName = driver.findElement(By.name("Nombre"));
-        elementName.sendKeys("Carlos");
+        WebElement elementTextTo = driver.findElement(By.id("text-to-visible"));
+        elementTextTo.sendKeys("Barcelona");
 
+        WebElement elementIdaVuelta = driver.findElement(By.id("ida-vuelta"));
+        elementIdaVuelta.click();
+
+        WebElement elementDiaSalida = driver.findElement(By.id("diaSalida"));
+        elementDiaSalida.sendKeys("11/05/2017");
+        elementDiaSalida.sendKeys("\ue007");
+
+        WebElement elementDiaRegreso = driver.findElement(By.id("diaRegreso"));
+        elementDiaRegreso.sendKeys("01/06/2017");
+        elementDiaRegreso.sendKeys("\ue007");
+
+        elementDiaSalida.sendKeys("\ue007");
+
+        Select selectAdultos = new Select(driver.findElement(By.id("select-adultos")));
+        selectAdultos.selectByVisibleText("2");
+
+        Select selectClase = new Select(driver.findElement(By.id("select-clase")));
+        selectClase.selectByVisibleText("Turista Premium");
+
+        WebElement elementCheckAvios = driver.findElement(By.id("check-avios"));
+        elementCheckAvios.click();
+
+        WebElement elementToAvailSubmit = driver.findElement(By.id("toAvailSubmit"));
+        elementToAvailSubmit.click();
 
 
     }
